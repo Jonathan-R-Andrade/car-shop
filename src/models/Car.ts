600910dae88fd0171b4044054d483067da3ad13a
Jonathan Reis de Andrade <jonathanreisdeandrade@gmail.com>
2022-09-14 16:53:33 -0300

    feat: adiciona model Car

---
+++ src/models/Car.ts
@@ -0,0 +1,21 @@
+import { model as mongooseCreateModel, Schema } from 'mongoose';
+import { ICar } from '../interfaces/ICar';
+import MongoModel from './MongoModel';
+
+const carMongooseSchema = new Schema<ICar>({
+  model: String,
+  year: Number,
+  color: String,
+  status: Boolean,
+  buyValue: Number,
+  doorsQty: Number,
+  seatsQty: Number,
+});
+
+class Car extends MongoModel<ICar> {
+  constructor(model = mongooseCreateModel('Car', carMongooseSchema)) {
+    super(model);
+  }
+}
+
+export default Car;
