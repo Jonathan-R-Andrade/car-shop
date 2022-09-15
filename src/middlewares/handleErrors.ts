00928c75e168c8b96c3956fc0860468c1080bc06
Jonathan Reis de Andrade <jonathanreisdeandrade@gmail.com>
2022-09-15 19:43:12 -0300

    refactor: altera resposta de erro da api
    altera a propriedade "message" para "error" do objeto de erro
    retornado pela api quando tem um erro

--- src/middlewares/handleErrors.ts
+++ src/middlewares/handleErrors.ts
@@ -10,14 +10,14 @@ const handleErrors: ErrorRequestHandler = (
   _next,
 ) => {
   if (err instanceof HttpError) {
-    return res.status(err.statusCode).json({ message: err.message });
+    return res.status(err.statusCode).json({ error: err.message });
   }
   if (err instanceof ZodError) {
-    return res.status(HttpStatus.BAD_REQUEST).json({ message: err.issues });
+    return res.status(HttpStatus.BAD_REQUEST).json({ error: err.issues });
   }
   return res
     .status(HttpStatus.INTERNAL_SERVER_ERROR)
-    .json({ message: 'Internal server error' });
+    .json({ error: 'Internal server error' });
 };
 
 export default handleErrors;
