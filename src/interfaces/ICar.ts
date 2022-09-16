740b92500b667621877f13873290a67751280818
Jonathan Reis de Andrade <jonathanreisdeandrade@gmail.com>
2022-09-16 14:37:24 -0300

    refactor: separa VehicleZodSchema de CarZodSchema

--- src/interfaces/ICar.ts
+++ src/interfaces/ICar.ts
@@ -1,20 +1,12 @@
 import { z } from 'zod';
-import { IVehicle } from './IVehicle';
+import { VehicleZodSchema } from './IVehicle';
 
 export const CarZodSchema = z.object({
-  model: z.string().min(3),
-  year: z.number().min(1900).max((new Date()).getFullYear()),
-  color: z.string().min(3),
-  status: z.boolean().optional(),
-  buyValue: z.number().positive(),
   doorsQty: z.number().min(2).max(4),
   seatsQty: z.number().min(2).max(7),
-});
+}).merge(VehicleZodSchema);
 
-export interface ICar extends IVehicle {
-  doorsQty: number,
-  seatsQty: number,
-}
+export type ICar = z.infer<typeof CarZodSchema>;
 
 export interface ICarWithId extends ICar {
   _id: string,
--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -1,3 +1,13 @@
+import { z } from 'zod';
+
+export const VehicleZodSchema = z.object({
+  model: z.string().min(3),
+  year: z.number().min(1900).max((new Date()).getFullYear()),
+  color: z.string().min(3),
+  status: z.boolean().optional(),
+  buyValue: z.number().positive(),
+});
+
 export interface IVehicle {
   model: string,
   year: number,
