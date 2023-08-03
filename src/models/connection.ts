1d6807da99ee56e552587b243e254b7c73a69807
Jonathan R. Andrade <jonathanreisdeandrade@gmail.com>
2023-08-03 20:00:59 -0300

    refactor: altera variável MONGO_DB_URL

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 import 'dotenv/config';
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/CarShop';
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://localhost:27017/CarShop';
 
 const connectToDatabase = (
   mongoDatabaseURI = process.env.MONGO_URI
