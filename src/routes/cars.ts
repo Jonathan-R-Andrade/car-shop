027c44e1bcd45838c0771bd65b9bd58f5db679ba
Jonathan R. Andrade <jonathanreisdeandrade@gmail.com>
2023-08-04 23:09:46 -0300

    refactor: encadeia rotas para reduzir a redundância

--- src/routes/cars.ts
+++ src/routes/cars.ts
@@ -9,10 +9,13 @@ const car = new CarModel();
 const carService = new CarService(car);
 const carController = new CarController(carService);
 
-carsRoute.post('/', (req, res) => carController.create(req, res));
-carsRoute.get('/', (req, res) => carController.read(req, res));
-carsRoute.get('/:id', (req, res) => carController.readOne(req, res));
-carsRoute.put('/:id', (req, res) => carController.update(req, res));
-carsRoute.delete('/:id', (req, res) => carController.delete(req, res));
+carsRoute.route('/')
+  .post((req, res) => carController.create(req, res))
+  .get((req, res) => carController.read(req, res));
+  
+carsRoute.route('/:id')
+  .get((req, res) => carController.readOne(req, res))
+  .put((req, res) => carController.update(req, res))
+  .delete((req, res) => carController.delete(req, res));
 
 export default carsRoute;
--- src/routes/motorcyclesRoute.ts
+++ src/routes/motorcyclesRoute.ts
@@ -9,10 +9,13 @@ const motorcycle = new MotorcycleModel();
 const motorcycleService = new MotorcycleService(motorcycle);
 const motorcycleController = new MotorcycleController(motorcycleService);
 
-motorcyclesRoute.post('/', (req, res) => motorcycleController.create(req, res));
-motorcyclesRoute.get('/', (req, res) => motorcycleController.read(req, res));
-motorcyclesRoute.get('/:id', (req, res) => motorcycleController.readOne(req, res));
-motorcyclesRoute.put('/:id', (req, res) => motorcycleController.update(req, res));
-motorcyclesRoute.delete('/:id', (req, res) => motorcycleController.delete(req, res));
+motorcyclesRoute.route('/')
+  .post((req, res) => motorcycleController.create(req, res))
+  .get((req, res) => motorcycleController.read(req, res));
+
+motorcyclesRoute.route('/:id')
+  .get((req, res) => motorcycleController.readOne(req, res))
+  .put((req, res) => motorcycleController.update(req, res))
+  .delete((req, res) => motorcycleController.delete(req, res));
 
 export default motorcyclesRoute;
